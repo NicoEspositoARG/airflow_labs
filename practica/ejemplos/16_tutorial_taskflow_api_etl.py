586d8cda84e867b781857827d7a3aba497f9fbe3
Nico Esposito <nico.esposito@gmail.com>
2021-09-23 19:31:34 +0000

    agregue dags ejemplo de Taskflow API

---
+++ practica/ejemplos/16_tutorial_taskflow_api_etl.py
@@ -0,0 +1,72 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+### TaskFlow API Tutorial Documentation
+This is a simple ETL data pipeline example which demonstrates the use of
+the TaskFlow API using three simple tasks for Extract, Transform, and Load.
+Documentation that goes along with the Airflow TaskFlow API tutorial is
+located
+[here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
+"""
+
+import json
+
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+
+default_args = {
+    "owner": "airflow",
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=["ejemplos"],
+    doc_md=__doc__,
+)
+def tutorial_taskflow_api_etl():
+    @task()
+    def extract():
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
+
+    @task(multiple_outputs=True)
+    def transform(order_data_dict: dict):
+        total_order_value = 0
+
+        for value in order_data_dict.values():
+            total_order_value += value
+
+        return {"total_order_value": total_order_value}
+
+    @task()
+    def load(total_order_value: float):
+        print(f"Total order value is: {total_order_value:.2f}")
+
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+
+
+tutorial_etl_dag = tutorial_taskflow_api_etl()
